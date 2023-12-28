03a4797f1358821870f5e187683a758eae69ac39
Ant <anthony.hillairet@gmail.com>
2023-12-28 08:10:28 -0800

    Add custom init: theme & hlsearch & format on save

---
+++ lua/custom/init.lua
@@ -0,0 +1,8 @@
+vim.cmd.colorscheme 'industry'
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
+vim.o.hlsearch = true
+
+-- https://www.jvt.me/posts/2022/03/01/neovim-format-on-save/
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
