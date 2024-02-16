685dc806ddb11cea96189f1cdfdba4bfcfef7b45
Ant <anthony.hillairet@gmail.com>
2024-02-16 11:10:14 -0800

    Handle treesitter breaking changes

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,4 +1,5 @@
 vim.cmd.colorscheme 'flexoki-dark'
+require("custom/treesitter-backward-compatibility")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
