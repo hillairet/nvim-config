c575f2a1dc926e19536365195341d5486d3b7b9c
ant @travelmate <anthony.hillairet@gmail.com>
2024-06-29 13:11:29 -0700

    Make onedark greys lighter

--- init.lua
+++ init.lua
@@ -196,13 +196,17 @@ require('lazy').setup({
   },
 
   {
-    --   'olimorris/onedarkpro.nvim',
     'navarasu/onedark.nvim',
     priority = 1000,
     config = true,
     opts = {
       style = 'deep',
       transparent = true,
+      colors = {
+        fg = "#c3cee3",
+        grey = "#6c7891",
+        light_grey = "#96a1b5",
+      },
       code_style = {
         comments = 'italic',
         keywords = 'italic',
