667fdac063c80e10240078a289902cbfba946e99
Ant <anthony.hillairet@gmail.com>
2024-06-29 04:05:09 -0700

    Fix ignore for flake8 error

--- init.lua
+++ init.lua
@@ -582,7 +582,7 @@ local servers = {
         yapf = { enabled = false },
         pyflakes = { enabled = false },
         mccabe = { enabled = false },
-        flake8 = { enabled = true, extend_ignore = { "E203" }, maxLineLength = 99 },
+        flake8 = { enabled = true, ignore = { "E203" }, maxLineLength = 99 },
         isort = { enabled = true, profile = "black", line_length = 99, multi_line_output = 3 },
         mypy = { enabled = true },
         black = { enabled = true, line_length = 99, skip_string_normalization = true },
