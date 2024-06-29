c7baa48777c58e4d9a8b4c16fea4df3b89d9a83a
ant @travelmate <anthony.hillairet@gmail.com>
2024-06-29 13:14:15 -0700

    Move theme import to theme file

--- init.lua
+++ init.lua
@@ -195,27 +195,7 @@ require('lazy').setup({
     },
   },
 
-  {
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = true,
-    opts = {
-      style = 'deep',
-      transparent = true,
-      colors = {
-        fg = "#c3cee3",
-        grey = "#6c7891",
-        light_grey = "#96a1b5",
-      },
-      code_style = {
-        comments = 'italic',
-        keywords = 'italic',
-        functions = 'italic',
-        strings = 'none',
-        variables = 'none',
-      },
-    },
-  },
+  require 'custom.plugins.theme',
 
   {
     -- Set lualine as statusline
