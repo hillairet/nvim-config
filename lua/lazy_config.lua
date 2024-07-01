f4e1f1c5c1ef620212fdf705ad47ca5844903461
ant @travelmate <anthony.hillairet@gmail.com>
2024-07-01 05:47:57 -0700

    Move indentation guide and status line to plugins

--- lua/lazy_config.lua
+++ lua/lazy_config.lua
@@ -45,27 +45,9 @@ require('lazy').setup({
 
   require 'custom.plugins.theme',
 
-  {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = true,
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
-  },
+  require 'plugins.status_line',
 
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },
+  require 'plugins.indentation_guide',
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim',  opts = {} },
