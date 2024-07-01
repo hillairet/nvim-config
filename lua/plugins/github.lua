96bda219e7e6c88341403136084c948df5c6ec88
ant @travelmate <anthony.hillairet@gmail.com>
2024-06-30 21:05:16 -0700

    Move all git packages to github plugin

--- lua/plugins/github.lua
+++ lua/plugins/github.lua
@@ -1,4 +1,8 @@
 return {
+  -- Git related plugins
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+
   -- Adds git related signs to the gutter, as well as utilities for managing changes
   'lewis6991/gitsigns.nvim',
   opts = {
