d0de014193da33e51d5ec28dbb65891a0c7bf5fb
Ant <anthony.hillairet@gmail.com>
2024-07-04 11:10:40 -0700

    Remove obsolete require custom

--- init.lua
+++ init.lua
@@ -55,8 +55,3 @@ require 'highlight_on_yank'
 
 -- [[ Configure LSP ]]
 require 'lsp_config'
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
-
-require 'custom'
