efa462c701d364ef49989790fbee222118bfeaa5
Ant <anthony.hillairet@gmail.com>
2024-07-03 22:43:54 -0700

    Move completion stuff out of custom/init

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -71,5 +71,29 @@ return {
         { name = 'path' },
       },
     }
+    -- `/` cmdline setup.
+    cmp.setup.cmdline('/', {
+      mapping = cmp.mapping.preset.cmdline(),
+      completion = { completeopt = 'menu,menuone,noselect' },
+      sources = {
+        { name = 'buffer' }
+      }
+    })
+
+    -- `:` cmdline setup.
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      completion = { completeopt = 'menu,menuone,noselect' },
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+        {
+          name = 'cmdline',
+          option = {
+            ignore_cmds = { 'Man', '!' }
+          }
+        }
+      })
+    })
   end
 }
