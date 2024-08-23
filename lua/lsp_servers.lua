8c986eb6e56dce722956f74f28cfa226b35901cd
ant @travelmate <anthony.hillairet@gmail.com>
2024-08-23 16:52:55 -0700

    Set the line length to the black default

--- lua/lsp_servers.lua
+++ lua/lsp_servers.lua
@@ -9,10 +9,10 @@ return {
         yapf = { enabled = false },
         pyflakes = { enabled = false },
         mccabe = { enabled = false },
-        flake8 = { enabled = true, ignore = { "E203" }, maxLineLength = 99 },
-        isort = { enabled = true, profile = "black", line_length = 99, multi_line_output = 3 },
+        flake8 = { enabled = true, ignore = { "E203" }, maxLineLength = 88 },
+        isort = { enabled = true, profile = "black", line_length = 88, multi_line_output = 3 },
         mypy = { enabled = true },
-        black = { enabled = true, line_length = 99, skip_string_normalization = true },
+        black = { enabled = true, line_length = 88 },
         ruff = { enabled = false },
         -- other plugins and settings...
       }
